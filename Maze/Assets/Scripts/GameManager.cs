e313390ff9584a647384ae552517aecfa9b9cff2
Leslie <249893977@qq.com>
2014-12-26 18:29:20 +0800

    Add Maze project
    Add Maze Project

---
+++ Maze/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public MazeController mazePrefab;
+	private MazeController mazeInstance;
+
+	// Use this for initialization
+	private void Start () {
+		BeginGame ();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			RestartGame();		
+		}
+	}
+
+	private void BeginGame(){
+		mazeInstance = Instantiate (mazePrefab) as MazeController;
+		StartCoroutine(mazeInstance.Generate ());
+	}
+
+	private void RestartGame(){
+		StopAllCoroutines ();
+		Destroy (mazeInstance.gameObject);
+		BeginGame ();
+	}
+}
